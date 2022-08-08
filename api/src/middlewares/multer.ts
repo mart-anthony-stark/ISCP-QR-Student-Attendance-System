72bfa7e93bb43587c4c2176c4d8adbb19f428c28
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-08 19:32:09 +0800

    add image upload

--- api/src/middlewares/multer.ts
+++ api/src/middlewares/multer.ts
@@ -1,21 +1,22 @@
-import { Request } from 'express'
-import multer, { FileFilterCallback } from 'multer'
-import path from 'path'
+import { Request } from "express";
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
 
-type DestinationCallback = (error: Error | null, destination: string) => void
-type FileNameCallback = (error: Error | null, filename: string) => void
+type DestinationCallback = (error: Error | null, destination: string) => void;
+type FileNameCallback = (error: Error | null, filename: string) => void;
 
-const storage = multer.diskStorage({
-  destination: (req:Request, file:any, cb:any) => {
+export const storage = multer.diskStorage({
+  destination: (req: Request, file: any, cb: any) => {
     cb(null, "images");
   },
-  filename: (req:any, file:any, cb:any) => {
-    const filename = Date.now() + path.extname(file.originalname);
+  filename: (req: any, file: any, cb: any) => {
+    const filename = `${Date.now()}_${Math.floor(
+      Math.random() * 9999
+    )}_${path.extname(file.originalname)}`;
     console.log(file);
     req.image = filename;
     cb(null, filename);
   },
 });
-const upload = multer({ storage });
 
-module.exports = upload;
+export const upload = multer({ storage });
