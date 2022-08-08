60643b06b39f149efba2a1632121f58b5199647f
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-08 10:23:06 +0800

    FIRST COMMIT

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, onUnmounted } from "@vue/runtime-core";
+import Header from "./components/Header.vue";
+
+// let counter;
+// onMounted(() => {
+//   counter = setInterval(() => {
+//     console.log("count");
+//   }, 1000);
+// });
+
+// onUnmounted(() => {
+//   clearInterval(counter);
+// });
+</script>
+
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+</style>
