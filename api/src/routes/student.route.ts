b5349658d29da84d9afe7904e96c2c06f0a10685
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-08 19:32:12 +0800

    add image upload

--- api/src/routes/student.route.ts
+++ api/src/routes/student.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { upload } from "../middlewares";
 import { catcher } from "../utils/catcher";
 const studentController = require("../controllers/student.controller");
 const router = Router();
 
 router.get("/", catcher(studentController.getAll));
 router.get("/:id", catcher(studentController.getOne));
-router.post("/", studentController.createOne);
+router.post("/", upload.single("image"), studentController.createOne);
 router.put("/:id", catcher(studentController.updateOne));
 router.delete("/:id", catcher(studentController.deleteOne));
 
