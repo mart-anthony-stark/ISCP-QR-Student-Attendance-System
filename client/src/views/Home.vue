c634f60f9aaa69d74caf1c0092644527bd2565a8
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-08 10:24:43 +0800

    FIRST COMMIT

---
+++ client/src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineAsyncComponent } from "@vue/runtime-core";
+import { useStore } from "vuex";
+import { computed, ref } from "vue";
+
+const Header = defineAsyncComponent(() => import("../components/Header.vue"));
+const store = useStore();
+
+const accessToken = computed(() => store.state.accessToken);
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
