88c0dcf31dc86f93794e2ac00fa74278099ceddd
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-08 10:24:19 +0800

    FIRST COMMIT

---
+++ client/src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { useRoute } from "vue-router";
+import AccountsDropdown from "./AccountsDropdown.vue";
+
+const route = useRoute();
+</script>
+
+<template>
+  <nav class="header">
+    <div class="flex gap-4 items-center">
+      <img
+        id="cnsc-logo"
+        class="w-14 h-14"
+        src="../assets/school.png"
+        alt="CNSC"
+      />
+
+      <router-link to="/home">
+        <button :class="{ active: route.name === 'home' }">Home</button>
+      </router-link>
+
+      <accounts-dropdown
+        :isActive="route.name == 'students' || route.name == 'admins'"
+      />
+
+      <router-link to="/attendance">
+        <button :class="{ active: route.name === 'attendance' }">
+          Attendance
+        </button>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+</style>
